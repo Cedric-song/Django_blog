e641c47b3503ae861601fb12cdcaee5263ffa8ae
Cedric-song <kengmun@126.com>
2015-08-10 00:48:09 +0800

    Myblog ver1.1 150809

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -5,6 +5,9 @@ from article.models import Article
 from datetime import datetime
 from django.http import Http404
 
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 # Create your views here.
 def home(request):
     post_list = Article.objects.all()  #获取全部的Article对象
@@ -25,4 +28,46 @@ def search_tag(request, tag) :
     return render(request, 'tag.html', {'post_list' : post_list})
 
 def about_me(request) :
-    return render(request, 'aboutme.html')    
+    return render(request, 'aboutme.html')
+
+def archives(request) :
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'archives.html', {'post_list' : post_list, 
+                                            'error' : False})
+
+def about_me(request) :
+    return render(request, 'aboutme.html')
+
+def blog_search(request):
+    if 's' in request.GET:
+        s = request.GET['s']
+        if not s:
+            return render(request,'home.html')
+        else:
+            post_list = Article.objects.filter(title__icontains = s)
+            if len(post_list) == 0 :
+                return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : True})
+            else :
+                return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : False})
+    return redirect('/')
+
+
+
+
+
+def home(request):
+    posts = Article.objects.all()  #获取全部的Article对象
+    paginator = Paginator(posts, 2) #每页显示两个
+    page = request.GET.get('page')
+    try :
+        post_list = paginator.page(page)
+    except PageNotAnInteger :
+        post_list = paginator.page(1)
+    except EmptyPage :
+        post_list = paginator.paginator(paginator.num_pages)
+    return render(request, 'home.html', {'post_list' : post_list})
--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -9,4 +9,7 @@ urlpatterns = patterns('',
     url(r'^$', 'article.views.home', name = 'home'),
     url(r'^(?P<id>\d+)/$', 'article.views.detail', name='detail'),
     url(r'^tag(?P<tag>\w+)/$', 'article.views.search_tag', name = 'search_tag'),
+    url(r'^archives/$', 'article.views.archives', name = 'archives'),
+    url(r'^aboutme/$', 'article.views.about_me', name = 'about_me'),
+    url(r'^search/$','article.views.blog_search', name = 'search'),            
 )
